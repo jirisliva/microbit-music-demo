3439474da41903b740fe4f4a6464708405f7dbed
Jiří Slíva <jiri.sliva@gmail.com>
2021-12-28 11:13:22 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,91 @@
+function ovecka () {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+}
+input.onButtonPressed(Button.A, function () {
+    vybrana_pisnicka += -1
+    basic.showNumber(vybrana_pisnicka)
+    zahrej(vybrana_pisnicka)
+})
+input.onButtonPressed(Button.B, function () {
+    vybrana_pisnicka += 1
+    basic.showNumber(vybrana_pisnicka)
+    zahrej(vybrana_pisnicka)
+})
+function zahrej (Pisnicka: number) {
+    if (Pisnicka == 0) {
+        basic.showString("?")
+        music.playMelody("E B C5 A B G A F ", 120)
+    } else if (Pisnicka == 1) {
+        basic.showIcon(IconNames.Heart)
+        ovecka()
+    } else if (Pisnicka == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+        music.playMelody("C5 A B G A F G E ", 120)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.SmallSquare)
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Skull)
+        basic.showIcon(IconNames.Ghost)
+        music.playMelody("B A G A G F A C5 ", 120)
+    }
+}
+let vybrana_pisnicka = 0
+music.setVolume(130)
+vybrana_pisnicka = 0
+basic.forever(function () {
+	
+})
