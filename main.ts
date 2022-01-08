c7afc53ba5214d005df03a8840963ece324a1017
Jiří Slíva <jiri.sliva@gmail.com>
2022-01-08 18:08:28 +0100

    pridani napovedy a animace

--- main.ts
+++ main.ts
@@ -49,23 +49,74 @@ function ovecka () {
     music.playTone(262, music.beat(BeatFraction.Quarter))
 }
 input.onButtonPressed(Button.A, function () {
-    vybrana_pisnicka += -1
-    basic.showNumber(vybrana_pisnicka)
-    zahrej(vybrana_pisnicka)
+    hraje_ovecka = 1
+    ovecka()
+    hraje_ovecka = 0
 })
+function ovecka2 () {
+    music.setTempo(60)
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(294, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+}
 input.onButtonPressed(Button.B, function () {
-    vybrana_pisnicka += 1
-    basic.showNumber(vybrana_pisnicka)
-    zahrej(vybrana_pisnicka)
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Fabulous)
+    basic.showIcon(IconNames.TShirt)
+    basic.showIcon(IconNames.No)
+    basic.showString("you died")
 })
 function zahrej (Pisnicka: number) {
     if (Pisnicka == 0) {
         basic.showString("?")
         music.playMelody("E B C5 A B G A F ", 120)
-    } else if (Pisnicka == 1) {
-        basic.showIcon(IconNames.Heart)
-        ovecka()
-    } else if (Pisnicka == 1) {
+    } else if (Pisnicka == 0) {
+        basic.showIcon(IconNames.Yes)
+    } else if (Pisnicka == 0) {
         basic.showIcon(IconNames.SmallSquare)
         music.playMelody("C5 A B G A F G E ", 120)
     } else {
@@ -84,9 +135,46 @@ function zahrej (Pisnicka: number) {
         music.playMelody("B A G A G F A C5 ", 120)
     }
 }
-let vybrana_pisnicka = 0
+let hraje_ovecka = 0
+let nehraje_ovecka = 1
 music.setVolume(130)
-vybrana_pisnicka = 0
-basic.forever(function () {
-	
+loops.everyInterval(100, function () {
+    if (hraje_ovecka == 1) {
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . # # # .
+            . # # # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . # # # .
+            . # # # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . # # # .
+            . # # # .
+            . # . # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
 })
